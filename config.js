790da9bf10332f181e2aab52cf13aaa3e11352a7
FedericoBarrios <fbarrios.smc@gmail.com>
2026-08-13 17:41:49 -0300

    planilla precios

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const CONFIG = {
      de las temporadas de este archivo. Si la planilla falla, también usa este archivo
      (nunca se rompe). Ejemplo:
      preciosSheetUrl: "https://docs.google.com/spreadsheets/d/e/XXXX/pub?output=csv", */
-  preciosSheetUrl: null,
+  preciosSheetUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRSed5TuhDwVMhAqQiz9O3xzXfcI4tNdQU7423GS89kYm20JxuPmzAdPcRP0DgImmqQtY1hJxnjXO-V/pub?output=csv",
 
   /* --- Meses en que la posada está CERRADA ---
      0=Ene 1=Feb 2=Mar 3=Abr 4=May 5=Jun 6=Jul 7=Ago 8=Sep 9=Oct 10=Nov 11=Dic
